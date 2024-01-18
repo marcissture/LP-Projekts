0b80014f84b1d59b29377bc5e4aec1a42980c34a
Krists Milenbergs <krists.mil@gmail.com>
2024-01-18 12:03:14 +0200

    uppdate

--- Projekts.py
+++ Projekts.py
@@ -1,43 +1,51 @@
 from os import system
-from turtle import clear
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import PySimpleGUI as sg
+import time
+from getpass import getpass
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import schedule
+import datetime
 # from selenium.webdriver.support.wait import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as ec
-import time
 # strftime(%d) iespejams vajag ar "%d"
 
-# Payload ievade
-usr = input("Lūdzu ievadīt lietotājvārdu: ")
-pwd = input("Lūdzu ievadīt paroli: ")
-system('clear')
+# # Payload ievade
+# usr = input("Lūdzu ievadīt lietotājvārdu: ")
+# pwd = getpass(prompt="Lūdzu ievadīt paroli: ")
+# system('clear')
 
+# Sākuma lapas atvēršana
 service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
-
-# Sākuma lapas atvēršana
 url = "https://estudijas.rtu.lv/"
 driver.get(url)
 time.sleep(2)
 
-# Pieslēgšanās
-find = driver.find_element(By.ID, "submit")
-find.click()
-find = driver.find_element(By.ID, "IDToken1")
-find.send_keys(usr)
-find = driver.find_element(By.ID, "IDToken2")
-find.send_keys(pwd)
-find = driver.find_element(By.NAME, "Login.Submit")
-find.click()
+def scrape(usr, pwd):
+    # Pieslēgšanās
+    find = driver.find_element(By.ID, "submit")
+    find.click()
+    find = driver.find_element(By.ID, "IDToken1")
+    find.send_keys(usr)
+    find = driver.find_element(By.ID, "IDToken2")
+    find.send_keys(pwd)
+    find = driver.find_element(By.NAME, "Login.Submit")
+    find.click()
+    find = driver.find_element(By.LINK_TEXT, "Atvērt kalendāru...")
+    find.click()
 
-# Izvilkšana
-find = find = driver.find_element(By.CLASS_NAME, "day-number")
-day = find.get_attribute("value")
-print(day)
-input()
+    # Izvilkšana
+    data = []
+    find = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[2]/div/div/section/div/div/div[1]/div/div[2]/div").text
+    data.append(find)
+    sdata = (s.split('\n') for s in data)
+    print(sdata) #sadalit info 2d masiva
 
 #paraug dati
 event_list = []
@@ -63,11 +71,11 @@ def log_in(): #iegust log in informāciju lai nolasītu no svarīgā konta
             payload = [values[0],values[1]] #log in dati
             return payload
 
-def period():# iegūst e-pastu uz kuru sūtīt info un laika periodu no kura nolasīt info
+def period():# iegūst e-pastu uz kuru sūtīt info #laika periodu no kura nolasīt info būtu bijis nice ja mēs izpildītu ātrāk
     sg.theme('LightBlue')
     layout = [  [sg.Text('Ievadat e-pastu un laika periodu')],
                 [sg.Text('e-pasts'), sg.InputText()],
-                [sg.CalendarButton('Līdz kuram datumam', target='-CAL1-', pad=None, key='-CAL1-', format=('%Y-%m-%d'))],
+                # [sg.CalendarButton('Līdz kuram datumam', target='-CAL1-', pad=None, key='-CAL1-', format=('%Y-%m-%d'))],
                 [sg.Button('Ok'), sg.Button('Cancel')] ]
 
     window = sg.Window('period', layout)
@@ -101,8 +109,40 @@ def display_info(page):#attēlo iegūtos datus ar opciju nosūtīt atgādinājum
                 display_info((page+1)) #rekursijas princips datu izskatīšanai
                 break
             except IndexError:
-                break
+                exit()
 
-log_in()
-period()
-display_info(page)
+def send_email(usr, pwd):
+    sender_email = 'ortusAtgadinajums@gmail.com'
+    sender_password = 'MstuKmil1!'
+    receiver_email = notif_email
+    subject = 'Atādinājums par darbu' #add name
+    body = 'Jums ir jaiesniedz' + + ' līdz' #add name and time
+
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
+
+    message.attach(MIMEText(body, 'plain'))
+
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+    except Exception as e:
+        return
+
+    scheduled_date = datetime(2024, 1, 20, 12, 0, 0)
+    schedule.day.at("12:00").do(send_email)
+
+# Keep the script running to allow the scheduler to work
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+    usr, pwd = log_in()
+    notif_email = period()
+    scrape(usr, pwd)
+    display_info(page)
